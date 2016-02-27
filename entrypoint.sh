10cfbd0e7252d329974a6486bea910d658ba5c53
西集 <ywfwj2008@163.com>
2016-02-27 15:43:35 +0800

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-service php-fpm start
-
 # Copy a default configuration into place if not present
 if ! [ -f /usr/local/tengine/conf/nginx.conf ]; then
   echo "daemon off;" >> "/usr/local/tengine/conf/nginx.conf"
