498bd8d8a04e260f0410905a000a310fdb7d4f07
西集 <ywfwj2008@163.com>
2016-06-30 09:17:53 +0800

    fixed bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ fi
 
 # optimize php.ini
 sed -i "s@^memory_limit.*@memory_limit = ${MEMORY_LIMIT}M@" $PHP_INSTALL_DIR/etc/php.ini
-sed -i "s@^opcache.memory_consumption.*@opcache.memory_consumption=$MEMORY_LIMIT@" $PHP_INSTALL_DIR/etc/php.ini
+sed -i "s@^opcache.memory_consumption.*@opcache.memory_consumption=$MEMORY_LIMIT@" $PHP_INSTALL_DIR/etc/php.d/ext-opcache.ini
 
 service php-fpm start
 
